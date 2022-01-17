354645fa186dfea7fe82d2fbb1de36e08a6e150d
Simon Duranowski <sduranowski1@gmail.com>
2022-01-17 00:19:07 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,54 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 import Header from './Header';
 import { BrowserRouter as Router, Switch, Route} from "react-router-dom";
 import TinderCards from "./TinderCards";
 import ChatScreen from "./ChatScreen";
-import SwipeButtons from "./SwipeButtons";
 import Friendslist from "./Friendslist";
 import Chats from "./Chats";
 import FbChat from './FbChat';
-import CoinsCard from "./CoinsCard";
 import LeftColumn from "./LeftColumn"
 import './App.css';
 
-
+import Login from "./Login";
+import Register from "./Register";
+import Profilee from "./Profilee";
 
 function App() {
-  const[state, setState] = React.useState(false);
-  const[current, setCurrent] = React.useState({});
-
-
-  const openChat =(f) => {
-    setState(true);
-    setCurrent(f)
-  }
-
-  const closeChat= () => {
-    setState(false);
-  }
-
 
   return (
     <div className="App">
-      <Router>
-        <Switch>
-          <Route path="/chat/:person">
-            <Header backButton="/chat" />
-            <ChatScreen />
-          </Route>
-          <Route path="/chat">
-            <Header backButton="/" />
-            <Chats />
-          </Route>
-          <Route path="/">
-            <Header />
-            <LeftColumn />
-            <TinderCards/>
-            <Friendslist openChat={openChat}/>
-            <FbChat state={state} current={current} closeChat={closeChat}/>
-          </Route>
-        </Switch>
-      </Router>
+        <Router>
+            <Switch>
+              <Route path="/chat/:person">
+                <Header backButton="/chat" />
+                <ChatScreen />
+              </Route>
+              <Route path="/chat">
+                <Header backButton="/" />
+                <Chats />
+              </Route>
+              <Route exact path="/login" component={() => <Login />  } />
+              <Route exact path="/register" component={() => <Register />  } />
+              <Route exact path="/" component={() => <Profilee authorized={true} />} />
+            </Switch>
+          </Router>
     </div>
   );
 };

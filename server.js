29749cb0d3fa87e0b88b6de713e018c40bd8fc1b
Simon Duranowski <sduranowski1@gmail.com>
2022-01-16 23:47:53 -0500

    Update server.js

--- server.js
+++ server.js
@@ -3,10 +3,15 @@ import mongoose from 'mongoose';
 import Cards from './dbCards.js';
 import Cors from 'cors'
 
+
 //app config
 const app = express();
 const port = process.env.PORT || 8003;
-const connection_url = "mongodb+srv://dating-app:<password>@cluster0.t1wjt.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+const connection_url = "mongodb+srv://dating-app:dating-app@cluster0.t1wjt.mongodb.net/tinderdb?retryWrites=true&w=majority"
+
+import User from './User.js';
+import jwt from 'jsonwebtoken';
+import bcrypt from 'bcryptjs'
 
 //middlewares
 app.use(express.json());
@@ -16,7 +21,7 @@ app.use(Cors());
 mongoose.connect(connection_url, {})
 
 //api endpoints
-app.get("/", (req, res) => res.status(200).send("HELLO CLEVER PROGRAMMERS!!"))
+app.get('/', (req, res) => res.status(200).send("HELLO CLEVER PROGRAMMERS!!"))
 
 app.post('/tinder/cards', (req, res) => {
     const dbCard = req.body;
@@ -31,16 +36,91 @@ app.post('/tinder/cards', (req, res) => {
 });
 
 app.get('/tinder/cards', (req, res) => {
-    const dbCard = req.body;
-
     Cards.find((err, data) => {
         if (err) {
             res.status(500).send(err);
         } else {
-            res.status(201).send(data);
+            res.status(200).send(data);
         }
     });
 });
+
+app.post('/api/register', async (req, res) => {
+    console.log(req.body)
+    try {
+		const newPassword = await bcrypt.hash(req.body.password, 10)
+		await User.create({
+			name: req.body.name,
+			email: req.body.email,
+			password: newPassword,
+		})
+		res.json({ status: 'ok' })
+	} catch (err) {
+		res.json({ status: 'error', error: 'Duplicate email' })
+	}
+})
+
+app.post('/api/login', async (req, res) => {
+	const user = await User.findOne({
+		email: req.body.email,
+	})
+
+	if (!user) {
+		return { status: 'error', error: 'Invalid login' }
+	}
+
+	const isPasswordValid = await bcrypt.compare(
+		req.body.password,
+		user.password
+	)
+
+	if (isPasswordValid) {
+		const token = jwt.sign(
+			{
+				name: user.name,
+				email: user.email,
+			},
+			'secret123'
+		)
+
+		return res.json({ status: 'ok', user: token })
+	} else {
+		return res.json({ status: 'error', user: false })
+	}
+})
+
+app.get('/api/quote', async (req, res) => {
+	const token = req.headers['x-access-token']
+
+	try {
+		const decoded = jwt.verify(token, 'secret123')
+		const email = decoded.email
+		const user = await User.findOne({ email: email })
+
+		return res.json({ status: 'ok', quote: user.quote })
+	} catch (error) {
+		console.log(error)
+		res.json({ status: 'error', error: 'invalid token' })
+	}
+})
+
+app.post('/api/quote', async (req, res) => {
+	const token = req.headers['x-access-token']
+
+	try {
+		const decoded = jwt.verify(token, 'secret123')
+		const email = decoded.email
+		await User.updateOne(
+			{ email: email },
+			{ $set: { quote: req.body.quote } }
+		)
+
+		return res.json({ status: 'ok' })
+	} catch (error) {
+		console.log(error)
+		res.json({ status: 'error', error: 'invalid token' })
+	}
+})
 
 //listener
 app.listen(port, () => console.log(`listening on localhost: ${port}`));
